adbbf6820e39b5b7e22123e2cff0f874ae1d8e10
erfan-git <naderi.jyc@gmail.com>
2020-07-23 23:56:53 +0430

    Initial commit

---
+++ app/src/main/java/com/example/game/controller/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.game.controller.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.game.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

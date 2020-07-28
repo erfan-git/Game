360aee6bb0e08c929b2af62f9501421048e5ad11
erfan-git <naderi.jyc@gmail.com>
2020-07-28 14:01:28 +0430

    create views for feragments add fragments to activity

--- app/src/main/java/com/example/game/controller/activities/MainActivity.java
+++ app/src/main/java/com/example/game/controller/activities/MainActivity.java
@@ -1,16 +1,69 @@
 package com.example.game.controller.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.game.R;
+import com.example.game.controller.fragments.RowFragment;
+import com.example.game.controller.fragments.TicTocToeFragment;
+
+import static com.example.game.R.*;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mButtonTic;
+    private Button mButtonRow;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+
+        findAllViews();
+
+        setClickListeners();
+
+//        Fragment fragment = fragmentManager.findFragmentById(id.frameLayout_container);
+//        if (fragment == null) {
+//            fragmentManager
+//                    .beginTransaction()
+//                    .add(id.frameLayout_container, createFragment())
+//                    .commit();
+//        }
+    }
+
+    private void setClickListeners() {
+        mButtonTic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentManager
+                        .beginTransaction()
+                        .add(id.frameLayout_container, new TicTocToeFragment())
+                        .commit();
+
+            }
+        });
+
+        mButtonRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragmentManager
+                        .beginTransaction()
+                        .add(id.frameLayout_container, new RowFragment())
+                        .commit();
+
+            }
+        });
+    }
+
+    private void findAllViews() {
+        mButtonTic = findViewById(R.id.button_ticTacToc);
+        mButtonTic = findViewById(id.button_4inARow);
     }
 }
---
+++ app/src/main/java/com/example/game/controller/fragments/TicTocToeFragment.java
@@ -0,0 +1,34 @@
+package com.example.game.controller.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.game.R;
+
+public class TicTocToeFragment extends Fragment {
+
+
+
+    public TicTocToeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ticTocToe,container,false);
+    }
+}
